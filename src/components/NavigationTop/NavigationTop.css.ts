e26420dce950696c73510ca91e4201dd1e1a0331
hamo-o <yheel369@gmail.com>
2024-05-09 21:35:19 +0900

    feat: add NavigationTop component

---
+++ src/components/NavigationTop/NavigationTop.css.ts
@@ -0,0 +1,20 @@
+import { style } from "@vanilla-extract/css";
+import { vars } from "@/styles/theme.css";
+
+export const container = style({
+  width: "100%",
+  height: 90,
+
+  padding: "0 3.25rem",
+
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+
+  background: vars.color.black,
+});
+
+export const form = style({
+  display: "flex",
+  gap: "1.5rem",
+});
