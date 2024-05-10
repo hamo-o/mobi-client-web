731dadc8566bfd38b50ee795d88c70b646b12daa
hamo-o <yheel369@gmail.com>
2024-05-10 21:29:26 +0900

    feat: Home -> Place 라우팅

---
+++ src/app/place/[id]/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import { Card, Figure, NavigationTop } from "@/components";
+
+const Place = ({ params }: { params: { id: string } }) => {
+  return (
+    <main>
+      <NavigationTop />
+    </main>
+  );
+};
+
+export default Place;
