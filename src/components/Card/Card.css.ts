bc29ad5e100b38fac618db47cc10d4c0a27b11d9
hamo-o <yheel369@gmail.com>
2024-05-10 19:51:32 +0900

    feat: add Card component

---
+++ src/components/Card/Card.css.ts
@@ -0,0 +1,45 @@
+import { style } from "@vanilla-extract/css";
+import { vars } from "@/styles/theme.css";
+
+export const container = style({
+  width: 597,
+  height: 146,
+  padding: "1.75rem 1.5em",
+
+  borderRadius: "1rem",
+  background: vars.color.gray_06,
+
+  display: "flex",
+  alignItems: "center",
+  gap: 20,
+});
+
+export const imageContainer = style({
+  width: 90,
+  height: 90,
+
+  borderRadius: "0.5rem",
+  background: vars.color.gray_05,
+
+  flexShrink: 0,
+});
+
+export const textContainer = style({
+  width: "100%",
+
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  gap: 4,
+});
+
+export const titleContainer = style({
+  color: vars.color.gray_01,
+});
+
+export const discriptionContainer = style({
+  display: "flex",
+  gap: 4,
+
+  color: vars.color.gray_03,
+});
