adaf50ec1be1471e4698a13105caf8eda022a4a9
hamo-o <yheel369@gmail.com>
2024-05-10 21:22:03 +0900

    feat: Home 페이지 퍼블리싱

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,64 @@
-import { Button } from '@/components';
+import { Card, Figure, NavigationTop } from "@/components";
+import { container, cardContainer } from "./home.css";
 
-export default function Home() {
+const Home = () => {
   return (
-    <main>
-      <Button text="버튼" />
+    <main className={container}>
+      <NavigationTop />
+      <div className={cardContainer}>
+        {[
+          {
+            title: "홍대",
+            discription: ["61m", "도로소통 서행"],
+            rightChild: <Figure text="여유" />,
+          },
+          {
+            title: "가로수길",
+            discription: ["61m", "도로소통 서행"],
+            rightChild: <Figure state="third" text="약간 붐빔" />,
+          },
+          {
+            title: "홍대",
+            discription: ["61m", "도로소통 서행"],
+            rightChild: <Figure text="여유" />,
+          },
+          {
+            title: "홍대",
+            discription: ["61m", "도로소통 서행"],
+            rightChild: <Figure text="여유" />,
+          },
+          {
+            title: "압구정 로데오",
+            discription: ["61m", "도로소통 서행"],
+            rightChild: <Figure state="forth" text="붐빔" />,
+          },
+          {
+            title: "가로수길",
+            discription: ["61m", "도로소통 서행"],
+            rightChild: <Figure state="third" text="약간 붐빔" />,
+          },
+          {
+            title: "홍대",
+            discription: ["61m", "도로소통 서행"],
+            rightChild: <Figure text="여유" />,
+          },
+          {
+            title: "압구정 로데오",
+            discription: ["61m", "도로소통 서행"],
+            rightChild: <Figure state="forth" text="붐빔" />,
+          },
+        ].map((place, key) => (
+          <Card
+            key={key}
+            image=""
+            title={place.title}
+            discriptions={place.discription}
+            rightChild={place.rightChild}
+          />
+        ))}
+      </div>
     </main>
   );
-}
+};
+
+export default Home;
