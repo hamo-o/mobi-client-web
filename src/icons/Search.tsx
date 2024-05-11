4755ad19fa86e33c87b733fbfc8b68b65b97441c
hamo-o <yheel369@gmail.com>
2024-05-11 16:02:57 +0900

    fix: svg 문법 변경

--- src/icons/Search.tsx
+++ src/icons/Search.tsx
@@ -8,8 +8,8 @@ export const Search = () => {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M2 8.1448C1.99956 5.21363 4.06975 2.69034 6.94452 2.1181C9.81928 1.54585 12.6979 3.08403 13.82 5.79193C14.9421 8.49984 13.995 11.6233 11.558 13.252C9.12103 14.8808 5.87287 14.5612 3.8 12.4888C2.64763 11.3368 2.00014 9.77422 2 8.1448Z"
         stroke="#656565"
         strokeWidth="1.5"
