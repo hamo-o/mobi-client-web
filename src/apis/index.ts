1616dcedb55ab9a7c2c8d1daec17dabf06932506
hamo-o <yheel369@gmail.com>
2024-09-22 17:46:37 +0900

    feat: base axios 만들기

---
+++ src/apis/index.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
+});
+
+export default apiClient;
