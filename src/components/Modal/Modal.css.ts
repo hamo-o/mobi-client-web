7112c3ab024544c7b7d89188544fc66b70eae140
hamo-o <yheel369@gmail.com>
2024-09-22 16:40:18 +0900

    fix: Modal css 변경 원복

--- src/components/Modal/Modal.css.ts
+++ src/components/Modal/Modal.css.ts
@@ -2,7 +2,7 @@ import { style } from "@vanilla-extract/css";
 import { vars } from "@/styles/theme.css";
 
 export const background = style({
-  position: "absolute",
+  position: "fixed",
   top: 0,
   left: 0,
   width: "100vw",
