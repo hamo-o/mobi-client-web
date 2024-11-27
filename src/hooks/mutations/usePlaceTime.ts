c85dd8cb2e471ee54704689d644b68f3dc9f04e2
hamo-o <yheel369@gmail.com>
2024-11-28 01:14:14 +0900

    feat: 방문하기 시 마이페이지 이동

--- src/hooks/mutations/usePlaceTime.ts
+++ src/hooks/mutations/usePlaceTime.ts
@@ -3,6 +3,7 @@ import { useMutation } from "@tanstack/react-query";
 import { BaseResponse, PlaceTimeRequestDto } from "@/types/dto";
 import { useQueryClient } from "@tanstack/react-query";
 import { timeKeys } from "@/constants/queryKeys";
+import { revalidateTagByName } from "@/utils/revalidateTagByName";
 
 export const usePlaceTime = (placeId: number) => {
   const queryClient = useQueryClient();
@@ -13,6 +14,7 @@ export const usePlaceTime = (placeId: number) => {
     onSuccess: (response: BaseResponse<null>) => {
       if (!response.success) return;
       queryClient.invalidateQueries({ queryKey: timeKeys.all });
+      revalidateTagByName("time");
       return response.success;
     },
     onError: (error: any) => {
